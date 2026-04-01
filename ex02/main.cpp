23e99496360a6ec376aaa0f3a25013cf383c93c5
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-04-01 14:00:22 +0200

    Add handling for unknown type in identify function

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -35,6 +35,8 @@ void identify(Base* p)
         std::cout << "B" << std::endl;
     else if (dynamic_cast<C*>(p))
         std::cout << "C" << std::endl;
+    else
+        std::cout << "Unknown type" << std::endl;
 }
 
 void identify(Base& p)
