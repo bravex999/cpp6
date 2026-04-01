385db9a225756552a3e44af5a9d6f7f63154ce78
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-04-01 14:06:35 +0200

    Add handling for unknown dynamic cast types

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -45,6 +45,7 @@ void identify(Base& p)
     {
         (void)dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
+        return;
     }
     catch (std::exception&)
     {
@@ -53,6 +54,7 @@ void identify(Base& p)
     {
         (void)dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
+        return;
     }
     catch (std::exception&)
     {
@@ -61,10 +63,12 @@ void identify(Base& p)
     {
         (void)dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
+        return;
     }
     catch (std::exception&)
     {
     }
+    std::cout << "Unknown type" << std::endl;
 }
 int main()
 {
